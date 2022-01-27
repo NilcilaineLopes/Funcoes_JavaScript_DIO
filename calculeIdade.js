ed4cb096f15306df656220d92c2955378b455b7d
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-01-27 20:31:20 -0300

    Atividades funções

---
+++ calculeIdade.js
@@ -0,0 +1,24 @@
+function calculaIdades(anos) {
+    return `Daqui a ${anos} anos, ${this.nome} terá ${
+
+    this.idade + anos 
+} anos de idade.`;
+}
+
+const pessoa1 = {
+    nome: 'Maria',
+    idade: 30,
+}
+
+const pessoa2 = {
+    nome: 'Carla',
+    idade: 26,
+}
+
+const animal = {
+    nome: 'fiona',
+    idade: 5,
+    raca: 'Pug',
+}
+
+console.log(calculaIdades.call(pessoa2, 30));
